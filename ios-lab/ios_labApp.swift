7820ee99003b209959862f1e0cc871deec842794
barry <superluckygoose@gmail.com>
2026-05-29 13:12:48 +0800

    Initial Commit

---
+++ ios-lab/ios_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_labApp.swift
+//  ios-lab
+//
+//  Created by Barry on 2026/5/29.
+//
+
+import SwiftUI
+
+@main
+struct ios_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
